cdf1ff63cec0268346707647ab106f27b10cc59c
RichiKennedy <richard.kennedy@hyperisland.se>
2022-06-24 00:02:23 +0200

    finishing functionalities
    finishing functionalities

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,95 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import BigTitle from "../subComponents/BigTitle";
+
+import LogoComponent from "../subComponents/LogoComponent";
+import ParticleComponents from "../subComponents/ParticleComponents";
+import PowerButton from "../subComponents/PowerButton";
+import SocialIcons from "../subComponents/SocialIcons";
+import { DarkTheme } from "./Themes";
+
+// import astronaut from "../assets/Images/spaceman.png";
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// const float = keyf`
+// 0% { transform: translateY(-10px)}
+// 50% { transform: translateY(15px) translateX(15px)}
+// 100% { transform: translateY(-10px)}
+
+// `;
+
+// const Spaceman = styled.div`
+//   position: absolute;
+//   top: 10%;
+//   right: 10%;
+//   width: 20vw;
+//   animation: ${float} 4s ease infinite;
+
+//   img {
+//     width: 100%;
+//     height: auto;
+//   }
+// `;
+
+const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 30vw;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(0.6rem + 1vw);
+  backdrop-filter: blur(4px);
+
+  /* position: absolute; */
+  /* left: calc(5rem + 5vw);
+  top: 10rem; */
+
+  font-family: "Ubuntu", monospace;
+`;
 
 const AboutPage = () => {
-  return <div>AboutPage Component/Page</div>;
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponents theme="dark" />
+
+        {/* <Spaceman>
+          <img src={astronaut} alt="" />
+        </Spaceman> */}
+        <Main>
+          I'm an Frontend developer student from Australia. Currently attending
+          HyperIsland in Stockholm, Sweden
+          <br />
+          <br />
+          I'm very interested in the whole frontend stack and learning new and
+          exciting concepts. I love solving complex problems and turning them
+          into beautiful websites.
+          <br />
+          <br />
+          When im not coding, I enjoy snowboarding, surfing and coffee tasting
+          in the city. Currently I'm on the lookout for an internship to
+          kickstart my career as a Frontend developer.
+        </Main>
+        <BigTitle text="ABOUT" top="3%" right="2%" />
+      </Box>
+    </ThemeProvider>
+  );
 };
 
 export default AboutPage;
--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { Github } from "./AllSvgs";
+import { motion } from "framer-motion";
 
-const Box = styled.div`
-  width: calc(10rem + 15vw);
-  height: 20rem;
+const Box = styled(motion.li)`
+  width: calc(10rem + 20vw);
+  height: 35rem;
   padding: 1rem;
   color: ${(props) => props.theme.text};
   border: 2px solid ${(props) => props.theme.text};
   backdrop-filter: blur(2px);
   box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.3);
-  cursor: pointer;
+
   display: inline-block;
   flex-direction: column;
   z-index: 5;
@@ -35,6 +38,10 @@ const Title = styled.h3`
   }
 `;
 
+const Description = styled.h4`
+  font-weight: 300;
+  padding-top: 1rem;
+`;
 const Image = styled.div`
   background-image: ${(props) => `url(${props.img})`};
   width: 100%;
@@ -52,6 +59,7 @@ const Image = styled.div`
 
 const HashTags = styled.div`
   padding: 0.5rem 0;
+  padding-top: 1rem;
 `;
 const Tag = styled.span`
   padding-right: 0.5rem;
@@ -61,19 +69,74 @@ const Date = styled.span`
   padding-bottom: 1rem;
 `;
 
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 1rem;
+`;
+const Link = styled(NavLink)`
+  background-color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.body};
+  text-decoration: none;
+  padding: 0.5rem calc(2rem + 2vw);
+  border-radius: 0 0 0 50px;
+  font-size: calc(1em + 0.5vw);
+
+  ${Box}:hover & {
+    background-color: ${(props) => props.theme.body};
+    color: ${(props) => props.theme.text};
+    cursor: pointer;
+  }
+`;
+
+const Git = styled(NavLink)`
+  color: inherit;
+  text-decoration: none;
+
+  ${Box}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+      cursor: pointer;
+    }
+  }
+`;
+
+// FRAMER-MOTION ANIMATION
+const item = {
+  hidden: {
+    scale: 0,
+  },
+  show: {
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+    },
+  },
+};
+
 const BlogComponent = (props) => {
-  const { name, tags, date, imgSrc } = props.blog;
+  const { name, description, tags, date, imgSrc, demo, github } = props.blog;
   return (
-    <Box to="/">
+    <Box to="/" varients={item}>
       <Image img={imgSrc} />
 
       <Title>{name}</Title>
+      <Description>{description}</Description>
       <HashTags>
         {tags.map((tag, id) => {
           return <Tag key={id}>#{tag}</Tag>;
         })}
       </HashTags>
       <Date>{date}</Date>
+      <Footer>
+        <Link to={{ pathname: `${demo}` }} target="_blank">
+          Visit
+        </Link>
+        <Git to={{ pathname: `${github}` }} target="_blank">
+          <Github width={30} height={30} />
+        </Git>
+      </Footer>
     </Box>
   );
 };
